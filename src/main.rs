0318d839dee92236e3490ace6f4b3db2d62381b8
mantotheale <alessiomantonico@gmail.com>
2026-08-18 17:56:51 +0900

    Starting to render

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+use wgpu::{CurrentSurfaceTexture, Features, TextureViewDescriptor};
 use winit::{
     application::ApplicationHandler,
     event::*,
@@ -8,15 +10,112 @@ use winit::{
 use winit::window::WindowId;
 
 #[derive(Debug)]
-pub struct InitializationError { }
+pub enum InitializationError {
+    CreateSurfaceError(wgpu::CreateSurfaceError),
+    AdapterError(wgpu::RequestAdapterError),
+    RequestDeviceError(wgpu::RequestDeviceError),
+    NoSRGBSurface
+}
+
+#[derive(Debug)]
+pub struct LostSurfaceError { }
 
 pub struct State {
-    window: Window
+    window: Arc<Window>,
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration
 }
 
 impl State {
     pub async fn new(window: Window) -> Result<Self, InitializationError> {
-        Ok(Self { window })
+        let window = Arc::new(window);
+
+        let instance = wgpu::Instance::new(
+            wgpu::InstanceDescriptor::new_with_display_handle(
+                Box::new(window.clone())
+            )
+        );
+        println!("{:?}", instance);
+
+        let surface = instance.create_surface(window.clone())
+            .map_err(|err| InitializationError::CreateSurfaceError(err))?;
+        println!("{:?}", surface);
+
+        let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+            apply_limit_buckets: true,
+        }).await.map_err(|err| InitializationError::AdapterError(err))?;
+        println!("{:?}", adapter);
+        println!("CIAO");
+
+        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            label: None,
+            required_features: Features::empty(),
+            required_limits: Default::default(),
+            experimental_features: Default::default(),
+            memory_hints: Default::default(),
+            trace: Default::default(),
+        }).await.map_err(|err| InitializationError::RequestDeviceError(err))?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        println!("{:?}", surface_caps);
+
+        let surface_format = *surface_caps.formats.iter()
+            .find(|f| f.is_srgb())
+            .ok_or(InitializationError::NoSRGBSurface)?;
+        println!("Surface format\n{:?}", surface_format);
+
+        let window_size = window.inner_size();
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            color_space: wgpu::SurfaceColorSpace::Auto,
+            width: window_size.width,
+            height: window_size.height,
+            present_mode: wgpu::PresentMode::AutoVsync,
+            desired_maximum_frame_latency: 2,
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &config);
+
+        Ok(Self { window, surface, device, queue, config })
+    }
+
+    pub fn resize(&mut self) {
+        let size = self.window.inner_size();
+        if size.width > 0 && size.height > 0 {
+            self.config.width = size.width;
+            self.config.height = size.height;
+            self.surface.configure(&self.device, &self.config);
+            println!("Size: {:?}", size)
+        }
+    }
+
+    pub fn render(&mut self) -> Result<(), LostSurfaceError> {
+        self.window.request_redraw();
+
+        let output = match self.surface.get_current_texture() {
+            CurrentSurfaceTexture::Success(t) | CurrentSurfaceTexture::Suboptimal(t) => t,
+            CurrentSurfaceTexture::Timeout |
+            CurrentSurfaceTexture::Occluded |
+            CurrentSurfaceTexture::Validation => return Ok(()),
+            CurrentSurfaceTexture::Outdated => {
+                self.surface.configure(&self.device, &self.config);
+                return Ok(());
+            }
+            CurrentSurfaceTexture::Lost => return Err(LostSurfaceError { })
+        };
+
+        let view = output.texture.create_view(&TextureViewDescriptor::default());
+
+        Ok(())
     }
 }
 
@@ -44,6 +143,7 @@ impl ApplicationHandler for App {
 
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(_) => if let Some(s) = &mut self.state { s.resize() },
             WindowEvent::KeyboardInput {
                 event: KeyEvent {
                     physical_key: PhysicalKey::Code(code),
@@ -60,5 +160,6 @@ impl ApplicationHandler for App {
 
 pub fn main() {
     let event_loop = EventLoop::new().expect("Couldn't build event loop");
-    event_loop.run_app(&mut App::new()).expect("Error running the app");
+    let mut app = App::new();
+    event_loop.run_app(&mut app).expect("Error running the app");
 }
